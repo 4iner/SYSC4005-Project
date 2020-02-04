a60a0064133ef663ef840cc3bdbff7a451cb6df5
4iner <foreingcode@gmail.com>
2020-02-04 17:49:31 -0500

    added data and workstation1 impl

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from classes.Buffer import Buffer
-
+from classes.Workstation1 import Workstation1
 def main():
     b = Buffer()
-    b.putItem("hello")
-    print(""+b.getItem())
-    print(str(b.putItem("hi")) +" "+str(b.putItem("hi")) + " " + str(b.putItem("hi")))
+    b.putItem("hi")
+    b.putItem("hs")
+    w = Workstation1(b)
+    w.start()
  
 if __name__ == "__main__":
     main()
