8f0a30341fbb337c85aab68cb091aa5e4e582167
4iner <mufasa@DESKTOP-69NHSHV.localdomain>
2020-02-06 01:52:59 -0500

    s

--- classes/Buffer.py
+++ classes/Buffer.py
@@ -6,26 +6,19 @@ class Buffer:
     def __init__(self):
         self.q = queue.Queue(3)
         self.qsize = 0
-        self.lock = threading.Lock()
     
     def getItem(self):
-            self.lock.acquire()
             if(self.qsize == 0):
-                self.lock.release()
                 return False
             item = self.q.get()
             self.qsize = self.qsize - 1
-            self.lock.release()
             return item
     
     def putItem(self, item):
-            self.lock.acquire()
             if self.q.qsize == 2:
-                self.lock.release()
                 return False
             self.q.put(item)
             self.qsize = self.qsize + 1
-            self.lock.release()
             return True
     def size(self):
         return self.qsize
